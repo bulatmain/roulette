591b3950497bb094fc072c5bb592d03224322f34
bulat <sinntexxx@gmail.com>
2023-09-12 19:44:41 +0300

    Fixed .gitinore and refactored fileReaderForRoulette.py

--- src/fileReaderForRoulette.py
+++ src/fileReaderForRoulette.py
@@ -1,31 +1,36 @@
 
 
 class fileReaderForRoulette:
+    names = []
+    checkedList = []
+
     def __init__(self):
         raise Exception('Unaible to create objects of this class')
 
     @classmethod
     def readNamesAndCheckedListFromFile(cls, fileName):
         file = open(fileName, 'r')
-        names = []
-        checkedList = []
 
         for line in file:
-            try:
-                n, c = line.split('-')
-                n = n.strip(' \n')
-                c = c.strip(' \n')
-                names.append(n)
-                checkedList.append(cls.strToBool(c))
-            except Exception  as e:
-                n = line.strip(' \n')
-                names.append(n)
-                checkedList.append(False)
-
+            cls.parseAndAppendLine(line)
 
         file.close()
 
-        return names, checkedList
+        return cls.names, cls.checkedList
+    
+    @classmethod
+    def parseAndAppendLine(cls, line):
+        try:
+            n, c = line.split('-')
+            n = n.strip(' \n')
+            c = cls.strToBool(c.strip(' \n'))
+        except Exception as e:
+            n = line.strip(' \n')
+            c = False
+        
+        cls.names.append(n)
+        cls.checkedList.append(c)
+
 
     def strToBool(s: str) -> bool:
         return s == '1' or s == 'True' or s == 'true'
